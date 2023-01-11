061f918f7156c8a06630c594e7dc7c394963dbfc
Nur Khamid <Khamid1196@gmail.com>
2023-01-11 22:15:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://khamid:*****@cluster0.gmwhz.mongodb.net/merng_app?retryWrites=true&w=majority',
+    MONGODB: 'mongodb+srv://khamid:MONGODB@cluster0.gmwhz.mongodb.net/merng_app?retryWrites=true&w=majority',
     SECRET_KEY: 'some very secret key'
 }
