2e480c8e95f0dd42e9aad797e1d47cb9380aa8b6
Nur Khamid <Khamid1196@gmail.com>
2023-01-11 21:28:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req, pubsub }) 
+    context: ({ req }) => ({ req, pubsub }),
+    cors: {
+        origin: "*", // <- allow request from all domains
+        credentials: true,  // <- enable CORS response for requests with credentials (cookies, http authentication)
+      },
 });
 
 mongoose
